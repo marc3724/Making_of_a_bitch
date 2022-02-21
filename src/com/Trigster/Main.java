272a893dd9a836ad82bc892d012b85a754314ee8
Marc3724 <marc402m@edu.easj.dk>
2022-02-21 11:39:28 +0100

    do it

--- src/com/Trigster/Main.java
+++ src/com/Trigster/Main.java
@@ -5,11 +5,15 @@ public class Main {
     public static void main(String[] args) {
         //write the next line of lyric
 
-        // Why you callin' at 11:30
-        // When you only wanna do me dirty? (Uh-huh-uh)
-        // But I hit right back 'cause you got that, that, yeah
-        // Why you always wanna act like lovers?…
-        // But you never wanna be each other's (uh-huh-uh)
-        // I say don't look back but I go right back, yeah
+        /* Why you callin' at 11:30
+         When you only wanna do me dirty? (Uh-huh-uh)
+         But I hit right back 'cause you got that, that, yeah
+
+         Why you always wanna act like lovers?…
+         But you never wanna be each other's (uh-huh-uh)
+         I say don't look back but I go right back, yeah
+
+         All of a sudden, I'm hypnotized
+         */
     }
 }
