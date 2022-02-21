1c97d11abb778928e85fb6f80b21e7ebb67bdbf4
DanWuPet <danx1698@edu.easj.dk>
2022-02-21 11:32:02 +0100

    Worker class

--- src/com/Trigster/WorkerBehave.java
+++ src/com/Trigster/WorkerBehave.java
@@ -1,7 +1,26 @@
 package com.Trigster;
 
+import java.util.Scanner;
+
 public class WorkerBehave {
     // Dan make a class that tells a "worker" how to behave
-    static String attitude;
 
+    static Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) {
+        behave();
+
+    }
+
+    public static void behave(){
+        System.out.println("Don't be lazy, do your work and always be on time.");
+        System.out.println("Did you understand me?" + "\n" + "y/n?");
+        String answer = scn.nextLine();
+        if(answer.equals("y")){
+            System.out.println("Good, then I don't have to keep telling you.");
+        }
+        else{
+            System.out.println("No? No!! Then I just have to tell you again!");
+            behave();
+        }
+    }
 }
