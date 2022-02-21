59bd11304ab1d8d9f4754235df1b7e09799721f8
DanWuPet <danx1698@edu.easj.dk>
2022-02-21 11:38:17 +0100

    Song

--- src/com/Trigster/Main.java
+++ src/com/Trigster/Main.java
@@ -8,7 +8,8 @@ public class Main {
         // Why you callin' at 11:30
         // When you only wanna do me dirty? (Uh-huh-uh)
         // But I hit right back 'cause you got that, that, yeah
-        //Why you always wanna act like lovers?…
+        // Why you always wanna act like lovers?…
         // But you never wanna be each other's (uh-huh-uh)
+        // I say don't look back but I go right back, yeah
     }
 }
